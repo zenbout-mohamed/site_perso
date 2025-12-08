7ba12495998b9824e047fe601b529bdeef1e058b
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-12-08 16:26:53 +0100

    modifications des fichiers react

--- deploy/frontend/src/App.jsx
+++ deploy/frontend/src/App.jsx
@@ -1,29 +1,12 @@
-import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-function Home() { const [products, setProducts] = useState([]);
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
 
-useEffect(() => {
-  fetch("http://localhost:3001/products")
-  .then(res => res.json())
-  .then(data => setProducts(data))
-  .catch(err => console.error("Erreur API :", err)); 
-}, []);
-return( 
-  Produits{products.length === 0 && <p>Chargement...</p>}
-    {products.map(p => (
-    <div key={p.id} style={{
-      border:"1px solid #ddd",
-      padding:"10px",
-      margin:"10px",
-      borderRadius:"8px"
-    }}>
-      <h3>{p.name}</h3>
-      <p>{p.description}</p>
-      <strong>{p.price} €</strong>
-    </div>
-  ))}
-</div>
-
-); }
-
-export default Home;
+export default App;
