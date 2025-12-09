0c6f00ef6ca8f04ae8ee1d50a1d2cc6d81acced6
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-12-09 11:27:15 +0100

    style css du fichier Product.jsx modifié

--- deploy/frontend/src/pages/Product.jsx
+++ deploy/frontend/src/pages/Product.jsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "./Product.css";
+import "./Product.css"; 
 
 function Product() {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
@@ -16,28 +16,15 @@ function Product() {
       .catch(err => console.error("Erreur API :", err));
   }, [id]);
 
-  if (!product) {
-    return <p>Chargement...</p>;
-  }
+  if (!product) return <p>Chargement...</p>;
 
   return (
-    <div style={{ maxWidth: "400px", margin: "30px" }}>
+    <div className="product-card">
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p><strong>{product.price} €</strong></p>
 
-      <button
-        style={{
-          marginTop: "20px",
-          padding: "12px 20px",
-          borderRadius: "8px",
-          border: "none",
-          background: "#4F8",
-          cursor: "pointer",
-          fontSize: "18px",
-          fontWeight: "bold",
-        }}
-      >
+      <button>
         Ajouter au panier
       </button>
     </div>
