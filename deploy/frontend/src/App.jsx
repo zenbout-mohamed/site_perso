6e5eee4fbe26c433c865036c598ad4f1fb8f3772
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-12-09 16:08:48 +0100

    mise à jour de la page App.jsx

--- deploy/frontend/src/App.jsx
+++ deploy/frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <button onClick={() => window.location.href = "/cart"}>🛒 Panier</button>
-      
+      <NavBar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
