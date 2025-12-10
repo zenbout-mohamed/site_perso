79446010f3fe6e8ae82667bebbde2a301526ce25
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-12-10 11:14:23 +0100

    modification du fichier NavBar.jsx

--- deploy/frontend/src/components/NavBar.jsx
+++ deploy/frontend/src/components/NavBar.jsx
@@ -1,19 +1,30 @@
-import { Link } from "react-router-dom";
-import "./NavBar.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user") || "null");
+
+  function handleLogout() {
+    localStorage.removeItem("user");
+    navigate("/");
+  }
+
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <Link to="/" className="navbar-logo">
-          🏠 Accueil
-        </Link>
-      </div>
+      <Link to="/">Boutique</Link>
+      <Link to="/cart">Panier</Link>
 
       <div className="navbar-right">
-        <Link to="/cart" className="nav-link">
-          🛒 Panier
-        </Link>
+        {user ? (
+          <>
+            <span style={{ marginRight: "10px" }}>
+              Bonjour, {user.name}
+            </span>
+            <button onClick={handleLogout}>Se déconnecter</button>
+          </>
+        ) : (
+          <Link to="/login">Se connecter</Link>
+        )}
       </div>
     </nav>
   );
